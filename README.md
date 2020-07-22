5083bc62f5ba3c2e5d5d2422e45c5681b3d7eeb3
Matt Wilde <wildmat@amazon.com>
2020-07-22 11:37:04 -0700

    it works

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+sudo pigpiod
+pigs s 18 <0 or 500 - 2500>
+
+
+```
---
+++ rstouch.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# servo_demo.py
+# 2016-10-07
+# Public Domain
+
+# servo_demo.py          # Send servo pulses to GPIO 4.
+# servo_demo.py 23 24 25 # Send servo pulses to GPIO 23, 24, 25.
+
+import sys
+import time
+import random
+import pigpio
+
+pi = pigpio.pi()
+
+NUM_GPIO = 32
+
+MIN_WIDTH = 1700
+MAX_WIDTH = 2000
+min_sec = float(sys.argv[1])
+ran_1 = float(sys.argv[2])
+ran_2 = float(sys.argv[3])
+
+while True:
+
+   try:
+
+        pi.set_servo_pulsewidth(18, MIN_WIDTH)
+        time.sleep(0.2)
+        pi.set_servo_pulsewidth(18, MAX_WIDTH)
+        time.sleep(min_sec)
+        time.sleep(random.uniform(ran_1,ran_2))
+
+   except KeyboardInterrupt:
+      break
+
+print("\nTidying up")
+pi.set_servo_pulsewidth(18, 0)
+
+pi.stop()
+
